e8ac227e9b45b15dd933a1bc76673d9dbb919a9c
ACharLuk <acharluk@gmail.com>
2017-03-15 14:56:06 +0100

    Add player!

---
+++ player.js
@@ -0,0 +1,15 @@
+function Player() {
+    this.angle = 0;
+    this.keys = {
+        left: LEFT_ARROW,
+        right: RIGHT_ARROW
+    }
+}
+
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.show = function() {
+    
+}
